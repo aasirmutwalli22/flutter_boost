dfff52e1adffd4ca31bf3c7f2391f1fe7827b0a5
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2020-11-21 22:32:33 +0530

    21/11/2020: added let widget

--- lib/let.dart
+++ lib/let.dart
@@ -4,14 +4,6 @@ typedef _Builder<T> = Widget Function(T);
 class Let<T> extends StatelessWidget{
   final T let;
   final _Builder<T> builder;
-  List<String> g;
   const Let({Key key, @required this.let, @required this.builder}) : super(key: key);
-  @override Widget build(BuildContext context) {
-    ListView.builder(
-      itemCount: g.length,
-      itemBuilder: (BuildContext context, int index) {  },
-
-    );
-    return builder(let);
-  }
+  @override Widget build(BuildContext context) => builder(let);
 }
