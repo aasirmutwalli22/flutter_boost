84755674e0b3d4be1d9bdffe1c089a3799738a38
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2020-11-22 11:13:46 +0530

    22/11/2020

--- lib/let.dart
+++ lib/let.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
-typedef _Builder<T> = Widget Function(T);
+typedef _Builder<T> = Widget Function(BuildContext, T);
 class Let<T> extends StatelessWidget{
   final T let;
   final _Builder<T> builder;
   const Let({Key key, @required this.let, @required this.builder}) : super(key: key);
-  @override Widget build(BuildContext context) => builder(let);
+  @override Widget build(BuildContext context) => builder(context, let);
 }
