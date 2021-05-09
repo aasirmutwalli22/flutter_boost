dfd860a1aaec173cf3ed5a1586305c90d4d1469a
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2021-05-09 16:32:00 +0530

    migrated to null safety

--- lib/let.dart
+++ lib/let.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 class Let<T> extends StatelessWidget {
   final T let;
   final Widget Function(T) builder;
-  const Let({@required this.let, @required this.builder});
+  const Let({required this.let, required this.builder});
   @override
   Widget build(_) => builder(let);
 }
