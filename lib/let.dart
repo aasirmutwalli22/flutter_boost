ef205bfd5056631015e9c69d2d218467ac5073c8
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2021-06-02 18:34:47 +0530

    let widget updated

--- lib/let.dart
+++ lib/let.dart
@@ -1,8 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+
+typedef LetWidgetBuilder<T> = ListTile Function(T let);
+
+/// uses
+/// ```dart
+/// class ExampleWidget extends StatelessWidget{
+///   @override
+///   Widget build(BuildContext context) => FutureBuilder(
+///     builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) => Let(
+///       let: snapshot.data as int,
+///       builder: (data) => ListTile(
+///         title: Text(data.toString()),
+///       ),
+///     ),
+///     future: Future.value(10,),
+///   );
+/// }
+/// ```
+///
 class Let<T> extends StatelessWidget {
   final T let;
-  final Widget Function(T) builder;
+  final LetWidgetBuilder<T> builder;
   const Let({required this.let, required this.builder});
   @override
   Widget build(_) => builder(let);
